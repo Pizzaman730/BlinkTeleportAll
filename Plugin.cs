e589fb32454bbd697eecdb1f8e851a055db1fc86
Pizzaman730 <110941122+Pizzaman730@users.noreply.github.com>
2024-07-11 18:02:26 -0400

    Add files via upload

---
+++ Plugin.cs
@@ -0,0 +1,38 @@
+﻿using BepInEx;
+using BoplFixedMath;
+using HarmonyLib;
+using System.Reflection;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+namespace BlinkTeleportAll
+{
+    [BepInPlugin("com.PizzaMan730.BlinkTeleportAll", "BlinkTeleportAll", "1.0.0")]
+    public class Plugin : BaseUnityPlugin
+    {
+        private void Awake()
+        {
+            Logger.LogInfo("BlinkTeleportAll has loaded!");
+
+            Harmony harmony = new Harmony("com.PizzaMan730.BlinkTeleportAll");
+
+
+            MethodInfo original = AccessTools.Method(typeof(QuantumTunnel), "Init");
+            MethodInfo patch = AccessTools.Method(typeof(myPatches), "Init_Patch");
+            harmony.Patch(original, new HarmonyMethod(patch));
+        }
+
+        public class myPatches
+        {
+            
+            public static bool Init_Patch(ref bool ___isPlayer)
+            {
+                ___isPlayer = true;
+                return true;
+            }
+        }
+    }
+}
+
+//dotnet build "C:\Users\ajarc\BoplMods\BlinkTeleportAll\BoplBattleTemplate.csproj"
